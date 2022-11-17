b208642784376a54ac1f350e6569f23067e8a533
SahsiSunny <sahsisunny@gmail.com>
2022-11-17 07:06:05 +0530

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ import "./App.css";
 const footer = () => {
      return (
           <div className="footer-section">
-               <p>Made with ❤️ by <a href="https://github.comsahsisunny">Sahsi Sunny</a></p>
+               <p>Made with ❤️ by <a href="https://github.com/sahsisunny">Sunny Sahsi</a></p>
           </div>
      );
 }
